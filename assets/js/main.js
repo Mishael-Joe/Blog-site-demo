628888a208f26a217732cd0348b2030e1091d97a
mishael joseph <mishaeljoe55@gmail.com>
2023-05-03 21:42:05 +0100

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,93 @@
+// Grab elements
+const selectElement = selector => {
+     const element = document.querySelector(selector);
+    // const element = selector;
+    if(element) return element;
+    throw new Error(`Something went wrong, make sure that ${selector} exists or is typed correctly.`)
+};
+ console.log(selectElement('.open-menu-icon'));
+//Nav styles on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if(this.scrollY >= 15) {
+        headerElement.classList.add('activated');
+    } else {
+        headerElement.classList.remove('activated');
+    }
+};
+
+window.addEventListener('scroll', scrollHeader)
+
+// Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
+
+// Open/Close search form popup
+const formOpenBtn = selectElement('#search-icon');
+const formCloseBtn = selectElement('#form-close-btn');
+const searchFormContainer = selectElement('.search-form-container');
+
+formOpenBtn.addEventListener('click', () => searchFormContainer.classList.add('activated'));
+formCloseBtn.addEventListener('click', () => searchFormContainer.classList.remove('activated'));
+// -- Close the search form popup on ESC keypress
+window.addEventListener('keyup', event => {
+    if (event.key === 'Escape') searchFormContainer.classList.remove('activated');
+})
+// Switch theme/add to local storage
+
+
+// (const themeToggleBtn = selectElement('#theme-toggle-btn');
+// const toggleTheme = () => {
+//     const theme = selectElement('body');
+//     theme.classList.toggle('light-theme');           // This is a style I used @ first or before & it work perfectly,
+//     themeToggleBtn.classList.toggle('activated');    // Buy I'm unable to add it to local storage.
+// }
+// themeToggleBtn.addEventListener('click', toggleTheme);)
+
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+if (currentTheme) {
+    bodyElement.classList.add('light-theme');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('light-theme');
+    if (bodyElement.classList.contains('light-theme')) {
+        localStorage.setItem('currentTheme', 'themeActive');
+    } else {
+        localStorage.removeItem('currentTheme');
+    }
+});
+
+// Swiper
+const swiper = new Swiper(".swiper", {
+    autoplay: {
+        delay: 3000
+    },
+    slidesPerview: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    breakpoints: {
+        700: {
+            slidesPerView: 2
+        },
+        1200: {
+            slidesPerView: 3
+        }
+    }
+});
